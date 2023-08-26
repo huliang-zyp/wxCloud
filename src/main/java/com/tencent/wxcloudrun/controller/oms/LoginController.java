5881926a6d40f58af5376bf75a70a9b9112e1ee9
huliang-zyp <1485605427@qq.com>
2023-08-26 16:52:43 +0800

    配置swagger

---
+++ src/main/java/com/tencent/wxcloudrun/controller/oms/LoginController.java
@@ -0,0 +1,9 @@
+package com.tencent.wxcloudrun.controller.oms;
+
+import com.tencent.wxcloudrun.config.ApiResponse;
+public class LoginController {
+
+    ApiResponse login(){
+        return null;
+    }
+}
